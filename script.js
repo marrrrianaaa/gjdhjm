b2f2544302f4b05d1702e5291a5c8ce302f17138
marrrrianaaaa <110055268+marrrrianaaa@users.noreply.github.com>
2022-07-27 21:01:02 +0400

    Delete script.js
    Signed-off-by: marrrrianaaaa <110055268+marrrrianaaa@users.noreply.github.com>

--- script.js
+++
@@ -1,7 +0,0 @@
-function imgSlider(anything){
-    document.querySelector('.starbucks').src=anything;
-}
-function changeCircleColor(coLor){
-    const circle = document.querySelector('.circle');
-    circle.style.background=coLor
-}
